ec5b8e68563bcaa9af04eb14488893f0afe0ad84
Alex Nichol <unixpickle@gmail.com>
2017-01-19 17:27:18 -0500

    implemented LogSoftmax

--- anydifftest/math_test.go
+++ anydifftest/math_test.go
@@ -19,3 +19,16 @@ func TestTanh(t *testing.T) {
 		ch.FullCheck(t)
 	})
 }
+
+func TestLogSoftmax(t *testing.T) {
+	runWithCreators(t, func(t *testing.T, c anyvec.Creator, prec float64) {
+		v := makeRandomVec(c, 18)
+		ch := &ResChecker{
+			F: func() anydiff.Res {
+				return anydiff.LogSoftmax(v, 6)
+			},
+			V: []*anydiff.Var{v},
+		}
+		ch.FullCheck(t)
+	})
+}
--- math.go
+++ math.go
@@ -33,3 +33,66 @@ func (t *tanhRes) Propagate(u anyvec.Vector, g Grad) {
 	u.Mul(down)
 	t.In.Propagate(u, g)
 }
+
+type logSoftmaxRes struct {
+	In        Res
+	ChunkSize int
+	OutVec    anyvec.Vector
+}
+
+// LogSoftmax computes the log of the softmax function for
+// each chunk in a packed list of chunks.
+// The chunk size must divide the vector length.
+func LogSoftmax(v Res, chunkSize int) Res {
+	if v.Output().Len()%chunkSize != 0 {
+		panic("chunk size must divide vector size")
+	}
+	out := v.Output().Copy()
+	anyvec.LogSoftmax(out, chunkSize)
+	return &logSoftmaxRes{
+		In:        v,
+		ChunkSize: chunkSize,
+		OutVec:    out,
+	}
+}
+
+func (l *logSoftmaxRes) Output() anyvec.Vector {
+	return l.OutVec
+}
+
+func (l *logSoftmaxRes) Vars() VarSet {
+	return l.In.Vars()
+}
+
+func (l *logSoftmaxRes) Propagate(u anyvec.Vector, g Grad) {
+	numBatch := u.Len() / l.ChunkSize
+
+	// For each batch, compute the negative sum of the
+	// upstream components.
+	oneVec := u.Creator().MakeVector(l.ChunkSize)
+	oneVec.AddScaler(u.Creator().MakeNumeric(1))
+	oneCol := &anyvec.Matrix{
+		Data: oneVec,
+		Rows: l.ChunkSize,
+		Cols: 1,
+	}
+	uMat := &anyvec.Matrix{
+		Data: u,
+		Rows: numBatch,
+		Cols: l.ChunkSize,
+	}
+	batchSums := &anyvec.Matrix{
+		Data: u.Creator().MakeVector(numBatch),
+		Rows: numBatch,
+		Cols: 1,
+	}
+	batchSums.Product(false, false, u.Creator().MakeNumeric(-1), uMat, oneCol,
+		u.Creator().MakeNumeric(0))
+
+	temp := l.OutVec.Copy()
+	anyvec.Exp(temp)
+	anyvec.ScaleChunks(temp, batchSums.Data)
+	u.Add(temp)
+
+	l.In.Propagate(u, g)
+}
