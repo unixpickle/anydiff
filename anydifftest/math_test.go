29717a685ccd56e36bc8c9232a4997021fe0561f
Alex Nichol <unixpickle@gmail.com>
2017-01-22 10:00:42 -0500

    uncover sigmoid instability

--- anydifftest/math_test.go
+++ anydifftest/math_test.go
@@ -1,17 +1,32 @@
 package anydifftest
 
 import (
+	"math"
 	"testing"
 
 	"github.com/unixpickle/anydiff"
 	"github.com/unixpickle/anyvec"
+	"github.com/unixpickle/anyvec/anyvec32"
 )
 
 func TestTanh(t *testing.T) {
 	testMathFunction(t, anydiff.Tanh)
 }
 
-func TestSigmoid(t *testing.T) {
+func TestSigmoidOut(t *testing.T) {
+	inVec := anyvec32.MakeVectorData([]float32{1000, -1000, 2, -2, 0})
+	inRes := anydiff.NewConst(inVec)
+	actual := anydiff.Sigmoid(inRes).Output().Data().([]float32)
+	expected := []float32{1, 0, 0.880797078, 0.119202922, 0.5}
+	for i, x := range expected {
+		a := actual[i]
+		if math.IsNaN(float64(a)) || math.Abs(float64(x-a)) > 1e-3 {
+			t.Errorf("expected %f but got %f", x, a)
+		}
+	}
+}
+
+func TestSigmoidProp(t *testing.T) {
 	testMathFunction(t, anydiff.Sigmoid)
 }
 
