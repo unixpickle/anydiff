eef898a6b314503ad478dce4c41a8fcfec99c9e5
Alex Nichol <unixpickle@gmail.com>
2017-02-07 11:45:09 -0500

    MapN utility for combining sequence lists

--- anydifftest/map_test.go
+++ anydifftest/map_test.go
@@ -47,3 +47,38 @@ func TestMap(t *testing.T) {
 		})
 	})
 }
+
+func TestMapN(t *testing.T) {
+	runWithCreators(t, func(t *testing.T, c anyvec.Creator, prec float64) {
+		inSeq, varList := makeBasicTestSeqs(c)
+
+		combVar := makeRandomVec(c, 12)
+		combiner := &anydiff.Matrix{
+			Data: combVar,
+			Rows: 2,
+			Cols: 6,
+		}
+		varList = append(varList, combVar)
+
+		ch := &SeqChecker{
+			F: func() anyseq.Seq {
+				reducedSeq := anyseq.Map(inSeq, func(v anydiff.Res, n int) anydiff.Res {
+					v = anydiff.Tanh(v)
+					mat1 := &anydiff.Matrix{
+						Data: v,
+						Rows: n,
+						Cols: 6,
+					}
+					return anydiff.MatMul(false, true, combiner, mat1).Data
+				})
+				return anyseq.MapN(func(n int, v ...anydiff.Res) anydiff.Res {
+					return anydiff.ScaleRepeated(v[1], v[0])
+				}, reducedSeq, inSeq)
+			},
+			V:     varList,
+			Prec:  prec * 2,
+			Delta: prec,
+		}
+		ch.FullCheck(t)
+	})
+}
--- anyseq/map.go
+++ anyseq/map.go
@@ -77,3 +77,114 @@ func (m *mapResult) Propagate(u []*Batch, g anydiff.Grad) {
 	}
 	m.In.Propagate(downstream, g)
 }
+
+type mapNResult struct {
+	In   []Seq
+	Pool [][]*anydiff.Var
+	Res  []anydiff.Res
+	Out  []*Batch
+	V    anydiff.VarSet
+}
+
+// MapN maps a batched function over each timestep of the
+// sequences in a differentiable manner.
+//
+// All the sequences must have the same shape.
+//
+// It is guaranteed that f will be called for each
+// timestep in order.
+//
+// For each output, f is passed a one anydiff.Res for each
+// sequence, containing the packed batch contents.
+// It is also passed the batch size, which must match
+// across all the sequences.
+//
+// The result of f must have a length divisible by n,
+// since said result is treated as a packed batch of size
+// n.
+func MapN(f func(n int, v ...anydiff.Res) anydiff.Res, s ...Seq) Seq {
+	if len(s) == 0 {
+		panic("must take at least one sequence")
+	}
+	pool := make([][]*anydiff.Var, len(s))
+	res := make([]anydiff.Res, len(s[0].Output()))
+	out := make([]*Batch, len(s[0].Output()))
+	allVars := anydiff.VarSet{}
+	for i, seq := range s {
+		if len(seq.Output()) != len(out) {
+			panic("input shape mismatch")
+		}
+		allVars = anydiff.MergeVarSets(allVars, seq.Vars())
+		pool[i] = make([]*anydiff.Var, len(seq.Output()))
+	}
+	for i := range out {
+		var reses []anydiff.Res
+		n := s[0].Output()[i].NumPresent()
+		present := s[0].Output()[i].Present
+		for j, seq := range s {
+			out := seq.Output()[i]
+			pool[j][i] = anydiff.NewVar(out.Packed)
+			if out.NumPresent() != n {
+				panic("input shape mismatch")
+			}
+			reses = append(reses, pool[j][i])
+		}
+		res[i] = f(n, reses...)
+		if res[i].Output().Len()%n != 0 {
+			panic("mapped function must give a batch of results")
+		}
+		out[i] = &Batch{Packed: res[i].Output(), Present: present}
+		allVars = anydiff.MergeVarSets(allVars, res[i].Vars())
+	}
+	for _, x := range pool {
+		for _, y := range x {
+			allVars.Del(y)
+		}
+	}
+	return &mapNResult{
+		In:   s,
+		Pool: pool,
+		Res:  res,
+		Out:  out,
+		V:    allVars,
+	}
+}
+
+func (m *mapNResult) Output() []*Batch {
+	return m.Out
+}
+
+func (m *mapNResult) Vars() anydiff.VarSet {
+	return m.V
+}
+
+func (m *mapNResult) Propagate(u []*Batch, g anydiff.Grad) {
+	downstream := make([][]*Batch, len(m.In))
+	for i, inSeq := range m.In {
+		if g.Intersects(inSeq.Vars()) {
+			downstream[i] = make([]*Batch, len(m.Pool[i]))
+			for j, x := range m.Pool[i] {
+				d := x.Vector.Creator().MakeVector(x.Vector.Len())
+				g[x] = d
+				downstream[i][j] = &Batch{Packed: d, Present: u[i].Present}
+			}
+		}
+	}
+
+	for i, o := range m.Res {
+		o.Propagate(u[i].Packed, g)
+	}
+
+	for i, down := range downstream {
+		if down != nil {
+			for _, x := range m.Pool[i] {
+				delete(g, x)
+			}
+		}
+	}
+	for i, down := range downstream {
+		if down != nil {
+			m.In[i].Propagate(down, g)
+		}
+	}
+}
