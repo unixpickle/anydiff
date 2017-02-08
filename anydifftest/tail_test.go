def8b8a4ad00ce73ce4c1f8b8d402e3954ef351e
Alex Nichol <unixpickle@gmail.com>
2017-02-07 20:56:51 -0500

    cleaner anyseq Tail test

--- anydifftest/tail_test.go
+++ anydifftest/tail_test.go
@@ -1,49 +1,42 @@
 package anydifftest
 
 import (
-	"math"
 	"testing"
 
 	"github.com/unixpickle/anydiff"
 	"github.com/unixpickle/anydiff/anyseq"
 	"github.com/unixpickle/anyvec"
-	"github.com/unixpickle/anyvec/anyvec32"
 )
 
 func TestTailOutput(t *testing.T) {
-	c := anyvec32.CurrentCreator()
-	inSeq := anyseq.ConstSeqList([][]anyvec.Vector{
-		{
-			c.MakeVectorData(c.MakeNumericList([]float64{1, 2})),
-			c.MakeVectorData(c.MakeNumericList([]float64{-2, 1})),
-			c.MakeVectorData(c.MakeNumericList([]float64{2, -1})),
-		},
-		{},
-		{
-			c.MakeVectorData(c.MakeNumericList([]float64{1, 2})),
-			c.MakeVectorData(c.MakeNumericList([]float64{0, 5})),
-		},
-		{
-			c.MakeVectorData(c.MakeNumericList([]float64{9, 1})),
-		},
-		{
-			c.MakeVectorData(c.MakeNumericList([]float64{1, 2})),
-			c.MakeVectorData(c.MakeNumericList([]float64{-2, 1})),
-			c.MakeVectorData(c.MakeNumericList([]float64{2, -1})),
-			c.MakeVectorData(c.MakeNumericList([]float64{-9, 0})),
-		},
-	})
-	actual := anyseq.Tail(inSeq).Output().Data().([]float32)
-	expected := []float32{2, -1, 0, 5, 9, 1, -9, 0}
-	if len(actual) != len(expected) {
-		t.Fatalf("expected length %d but got %d", len(expected), len(actual))
-	}
-	for i, x := range expected {
-		a := actual[i]
-		if math.Abs(float64(x-a)) > 1e-3 || math.IsNaN(float64(a)) {
-			t.Errorf("output %d: expected %f but got %f", i, x, a)
+	runWithCreators(t, func(t *testing.T, c anyvec.Creator, prec float64) {
+		inSeq := anyseq.ConstSeqList([][]anyvec.Vector{
+			{
+				c.MakeVectorData(c.MakeNumericList([]float64{1, 2})),
+				c.MakeVectorData(c.MakeNumericList([]float64{-2, 1})),
+				c.MakeVectorData(c.MakeNumericList([]float64{2, -1})),
+			},
+			{},
+			{
+				c.MakeVectorData(c.MakeNumericList([]float64{1, 2})),
+				c.MakeVectorData(c.MakeNumericList([]float64{0, 5})),
+			},
+			{
+				c.MakeVectorData(c.MakeNumericList([]float64{9, 1})),
+			},
+			{
+				c.MakeVectorData(c.MakeNumericList([]float64{1, 2})),
+				c.MakeVectorData(c.MakeNumericList([]float64{-2, 1})),
+				c.MakeVectorData(c.MakeNumericList([]float64{2, -1})),
+				c.MakeVectorData(c.MakeNumericList([]float64{-9, 0})),
+			},
+		})
+		actual := getComponents(anyseq.Tail(inSeq).Output())
+		expected := []float64{2, -1, 0, 5, 9, 1, -9, 0}
+		if !vectorsClose(actual, expected, prec) {
+			t.Errorf("expected %v but got %v", expected, actual)
 		}
-	}
+	})
 }
 
 func TestTail(t *testing.T) {
