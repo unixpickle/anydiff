8d2a1e9801a339b5475dd65172e8ceaedaa00ac8
Alex Nichol <unixpickle@gmail.com>
2017-04-24 15:25:59 -0400

    added MakeFwd helper for anyfwd

---
+++ anyfwd/fwd_maker.go
@@ -0,0 +1,37 @@
+package anyfwd
+
+import "github.com/unixpickle/anydiff"
+
+// A FwdMaker is an object which can convert itself to use
+// forward auto-diff.
+//
+// Typically, the object is some kind of parametric model
+// and MakeFwd wraps the parameters in *Vectors.
+type FwdMaker interface {
+	MakeFwd(c *Creator)
+}
+
+// Parameterizer is an object with a set of parameters.
+//
+// This is used as a fall-back for MakeFwd.
+type Parameterizer interface {
+	Parameters() []*anydiff.Var
+}
+
+// MakeFwd promotes an object to use forward auto-diff
+// with the given Creator.
+//
+// If the object does not implement FwdMaker, this falls
+// back on Parameterizer.
+// If neither interface is available, this is a no-op.
+func MakeFwd(c *Creator, obj interface{}) {
+	if fm, ok := obj.(FwdMaker); ok {
+		fm.MakeFwd(c)
+	} else if p, ok := obj.(Parameterizer); ok {
+		for _, param := range p.Parameters() {
+			oldVec := param.Vector
+			param.Vector = c.MakeVector(oldVec.Len())
+			param.Vector.(*Vector).Values.Set(oldVec)
+		}
+	}
+}
